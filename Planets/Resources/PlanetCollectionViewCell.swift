2f0b79db79cbdf616d845308adc24a74415d9336
Stephanie Bowles <sbowl001@fiu.edu>
2019-05-16 10:11:11 +0800

    set up interface

---
+++ Planets/Resources/PlanetCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Stephanie Bowles on 5/16/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
